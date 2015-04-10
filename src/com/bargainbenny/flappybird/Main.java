f3762fb24aa62df04891a21cabc99ff445cf718d
bargainbenny <owen.charland@gmail.com>
2015-04-09 22:00:38 -0700

    Addition of LWJGL

---
+++ src/com/bargainbenny/flappybird/Main.java
@@ -0,0 +1,23 @@
+package com.bargainbenny.flappybird;
+
+public class Main {
+	
+	private int width = 1280;
+	private int height = 720;
+	
+	public void start()
+	{
+		
+	}
+	
+	public void run()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		new Main().start();
+	}
+
+}
